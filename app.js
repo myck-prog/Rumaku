ade49b5d3e9b4e1ba94c2c0c4c6a6776c0cb8aa3
myck-prog <myckland.learnprog@gmail.com>
2023-01-23 20:54:17 -0800

    Delete Features

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
-const ejsMate = require('ejs-mate');
+const ejsMate = require("ejs-mate");
+const session = require("express-session");
 const methodOverride = require("method-override");
+const passport = require("passport");
 const Rumaku = require("./models/rumaku");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
+const Review = require("./models/review");
 
 mongoose.connect("mongodb://localhost:27017/rumaku", {
   useNewUrlParser: true,
@@ -19,12 +24,39 @@ db.once("open", () => {
 
 const app = express();
 // using engine
-app.engine('ejs', ejsMate)
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true })); //parsing the url body
 app.use(methodOverride("_method"));
+
+const sessionConfig = {
+  secret: "thisshouldbeabettersecret!",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+app.use(session(sessionConfig));
+
+// Passport handling using passport library
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Fake user trial
+app.get("/fakeUser", async (req, res) => {
+  const user = new User({ email: "joko@gmail.com", username: "jokkk" });
+  const newUser = await User.register(user, "chicken");
+  res.send(newUser);
+});
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -45,7 +77,7 @@ app.post("/rumakus", async (req, res) => {
 });
 
 app.get("/rumakus/:id", async (req, res) => {
-  const rumaku = await Rumaku.findById(req.params.id);
+  const rumaku = await Rumaku.findById(req.params.id).populate("reviews");
   res.render("rumakus/show", { rumaku });
 });
 // Edit Functionality
@@ -64,6 +96,22 @@ app.delete("/rumakus/:id", async (req, res) => {
   const { id } = req.params;
   await Rumaku.findByIdAndDelete(id);
   res.redirect("/rumakus");
+});
+
+app.post("/rumakus/:id/reviews", async (req, res) => {
+  const rumaku = await Rumaku.findById(req.params.id);
+  const review = new Review(req.body.review);
+  rumaku.reviews.push(review);
+  await review.save();
+  await rumaku.save();
+  res.redirect(`/rumakus/${rumaku._id}`);
+});
+
+app.delete("/rumakus/:id/reviews/:reviewId", async (req, res) => {
+  const { id, reviewId } = req.params;
+  await Rumaku.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+  await Review.findByIdAndDelete(req.params.reviewId);
+  res.redirect(`/rumakus/${id}`);
 });
 
 // Render
--- models/rumaku.js
+++ models/rumaku.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Review = require("./review");
 const Schema = mongoose.Schema;
 
 const RumakuSchema = new Schema({
@@ -7,6 +8,22 @@ const RumakuSchema = new Schema({
   price: Number,
   description: String,
   location: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
+});
+
+RumakuSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews
+      },
+    });
+  }
 });
 
 module.exports = mongoose.model("Rumaku", RumakuSchema);
